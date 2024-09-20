feb821f6163275ac95f569659d05879d4bd42662
shahil <shahilsk0111@gmail.com>
2024-09-20 21:50:55 +0530

    Updated header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,35 +1,64 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react'
 import React from 'react'
-import { Link , useLocation } from 'react-router-dom'
-import {AiOutlineSearch} from "react-icons/ai"
-import {FaMoon} from "react-icons/fa"
+import { Link, useLocation } from 'react-router-dom'
+import { AiOutlineSearch } from "react-icons/ai"
+import { FaMoon } from "react-icons/fa"
+import { useSelector } from "react-redux"
 
 export default function Header() {
 
     const path = useLocation().pathname;
-  return (
-    <Navbar className='border-b-2'>
-        <Link to="/" className='self-center text-sm sm:text-xl font-semibold dark:text-white'>
-            <div className='px-2 py-1 bg-blue-800 rounded-lg text-white'>BlogMania</div>
-        </Link>
+    const { currentUser } = useSelector(state => state.user)
 
-        <form >
-            <TextInput type='text' placeholder='search' rightIcon={AiOutlineSearch} className='hidden lg:inline'/>
-        </form>
-        <Button className='w-12 h-10  lg:hidden'color="gray" pill><AiOutlineSearch/></Button>
-
-        <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline ' color="gray" pill>
-                <FaMoon/>
-            </Button>
-
-            <Link to="/sign-in">
-                <Button className='bg-blue-800' outline >
-                    Sign In
-                </Button>
+    return (
+        <Navbar className='border-b-2'>
+            <Link to="/" className='self-center text-sm sm:text-xl font-semibold dark:text-white'>
+                <div className='px-2 py-1 bg-blue-800 rounded-lg text-white'>BlogMania</div>
             </Link>
-            <Navbar.Toggle/>
-        </div>
+
+            <form >
+                <TextInput type='text' placeholder='search' rightIcon={AiOutlineSearch} className='hidden lg:inline' />
+            </form>
+            <Button className='w-12 h-10  lg:hidden' color="gray" pill><AiOutlineSearch /></Button>
+
+            <div className='flex gap-2 md:order-2'>
+                <Button className='w-12 h-10 hidden sm:inline ' color="gray" pill>
+                    <FaMoon />
+                </Button>
+                {
+                    currentUser? (
+                        <Dropdown arrowIcon={false} inline label={
+                            <Avatar rounded img={currentUser.profilePicture} alt="user" />
+                            }>
+                        <Dropdown.Header>
+                            <span className='block text-sm'>
+                                @{currentUser.username} 
+                            </span>
+                            <span className='block text-sm font-medium truncate'>
+                                @{currentUser.email} 
+                            </span>
+                        </Dropdown.Header>
+                            <Link to={'/dashboard?tab=profile'}>
+                            <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+
+                        <Dropdown.Divider/>
+                        <Dropdown.Item>Sign Out</Dropdown.Item>
+
+                        </Dropdown>
+                    ) :
+                     (
+                        <Link to="/sign-in">
+                        <Button className='bg-blue-800' outline >
+                            Sign In
+                        </Button>
+                    </Link>
+                    )
+                        
+                }
+               
+                <Navbar.Toggle />
+            </div>
             <Navbar.Collapse>
                 <Navbar.Link active={path === "/"} as={'div'}>
                     <Link to="/">Home</Link>
@@ -41,6 +70,6 @@ export default function Header() {
                     <Link to="/projects">Projects</Link>
                 </Navbar.Link>
             </Navbar.Collapse>
-    </Navbar>
-  )
+        </Navbar>
+    )
 }
