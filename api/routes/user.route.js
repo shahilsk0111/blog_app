2f6ad2c97e81e9755e200e46cc1674420302a8a3
shahil <shahilsk0111@gmail.com>
2024-09-22 14:17:32 +0530

    complete post page functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,21 @@
-import express from "express";
-import { deleteUser, test, updateUser , signout } from "../controllers/user.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+import express from 'express';
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  signout,
+  test,
+  updateUser,
+} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
-router.put('/update/:userId' ,verifyToken ,  updateUser)
-router.delete('/delete/:userId' ,verifyToken , deleteUser)
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
