d00c3f558a681855fdf00e92717cf258bfeadf71
shahil <shahilsk0111@gmail.com>
2024-09-22 21:46:04 +0530

    add preinstallation for hosting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from "./pages/CreatePost"
 import UpdatePost from "./pages/UpdatePost"
 import PostPage from "./pages/PostPage"
 import ScrollToTop from "./components/ScrollToTop"
+import Search from "./pages/Search"
 
 
 function App() {
